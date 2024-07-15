f7e25b89c27810ec27d812aaf7a814308c9897d7
abdul kadir <101203829+mdkadir360@users.noreply.github.com>
2024-07-15 23:01:38 +0530

    Update main.tf

--- 12-Sqldatabase/main.tf
+++ 12-Sqldatabase/main.tf
@@ -7,11 +7,11 @@ resource "azurerm_mssql_database" "datablock" {
     for_each = var.sqldata
   name           = each.value.name
   server_id      = data.azurerm_mssql_server.serdata[each.key].id
-#   collation      = "SQL_Latin1_General_CP1_CI_AS"
-#   license_type   = "LicenseIncluded"
-#   max_size_gb    = 4
-#   read_scale     = true
-#   sku_name       = "S0"
-#   zone_redundant = true
-#   enclave_type   = "VBS"
+   collation      = "SQL_Latin1_General_CP1_CI_AS"
+   license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+   read_scale     = true
+   sku_name       = "S0"
+   zone_redundant = true
+   enclave_type   = "VBS"
 }
